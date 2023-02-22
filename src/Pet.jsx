f074d8abbe193f9af43067ac57b957a17a3b2043
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-21 19:06:52 -0600

    refactored pet component to jsx

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,13 +1,5 @@
 import React from "react";
 
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, props.name),
-//       React.createElement("h2", {}, props.animal),
-//       React.createElement("h2", {}, props.breed),
-//     ]);
-//   };
-
 const Pet = (props) => {
     return (
         <div>
