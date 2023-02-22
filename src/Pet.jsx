5e180aafe0aa1e606abdeb038d5ee7e2ae18120a
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-22 09:02:31 -0600

    app not rendering

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 const Pet = (props) => {
     return (
-        <div>
-            <h1>{props.name}</h1>
-            <h2>{props.animal}</h2>
-            <h2>{props.breed}</h2>
-        </div>
+      <div>
+        <h1>{props.name}</h1>
+        <h2>{props.animal}</h2>
+        <h2>{props.breed}</h2>
+      </div>
     );
-};
-
-
+  };
+  
   export default Pet;
