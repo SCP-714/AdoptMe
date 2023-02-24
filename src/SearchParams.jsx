012078c5ebf9bf75f9ad2312fc5c4041849e9edc
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-24 10:24:40 -0600

    added animal selector

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useState } from "react";
 
+const ANIMALS = ["brid", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
-  const [location, setLocation] = useState("Nashville, TN");
+  let [location, setLocation] = useState("Nashville, TN");
+  let [animal, setAnimal] = useState("");
+
   return (
     <div className="search-params">
       <form>
@@ -15,11 +19,25 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+                <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
         <button>Submit</button>
       </form>
     </div>
   );
 };
-
 
 export default SearchParams;
