d61070ac527b52af3a2d18a920da0b2899be9230
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-24 10:08:45 -0600

    search component in working state

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,4 +1,5 @@
-import { React, useState } from "react";
+import React from "react";
+import { useState } from "react";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Nashville, TN");
