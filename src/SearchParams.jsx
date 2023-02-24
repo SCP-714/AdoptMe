9d8caee977c968e8f190a35b5d002f5a4e46ab43
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-23 23:44:40 -0600

    added useState

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import { React, useState } from "react";
+
 const SearchParams = () => {
-    const location = "Seattle, WA";
-    return (
-      <div className="search-params">
-        <form>
-          <label htmlFor="location">
-            Location
-            <input id="location" value={location} placeholder="Location" />
-          </label>
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  };
-  
-  export default SearchParams;
+  const [location, setLocation] = useState("Nashville, TN");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
