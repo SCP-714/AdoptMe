e45afc6f8c013bb9fd02710b09df232a1c41d0da
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-28 16:58:44 -0600

    adding Results component

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,8 +1,27 @@
 import Pet from "./Pet";
 
-const Results = ({ Pets }) => {
+const Results = ({ pets }) => {
     return (
-        <div className="search">
-            {}
-        </div>
-}
+      <div className="search">
+        {!pets.length ? (
+          <h1>No Pets Found</h1>
+        ) : (
+          pets.map((pet) => {
+            return (
+              <Pet
+                animal={pet.animal}
+                key={pet.id}
+                name={pet.name}
+                breed={pet.breed}
+                images={pet.images}
+                location={`${pet.city}, ${pet.state}`}
+                id={pet.id}
+              />
+            );
+          })
+        )}
+      </div>
+    );
+  };
+  
+  export default Results;
