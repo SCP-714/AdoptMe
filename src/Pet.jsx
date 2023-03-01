8a51589e82d63720dfc070b12f7ebb850fde0fe0
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-02-28 18:52:27 -0600

    added styling to pets

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-const Pet = (props) => {
-    return (
-      <div>
-        <h1>{props.name}</h1>
-        <h2>{props.animal}</h2>
-        <h2>{props.breed}</h2>
-      </div>
+const Pet = ({name, animal, breed, images, location, id}) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg"
+  if (images.length){
+    hero = images[0];
+  }   
+
+  return (
+      <a href={`/details/${id}`} className="pet">
+        <div className="image-container">
+          <img src ={hero} alt={name} />
+        </div>
+        <div className="info">
+         <h1>{name}</h1>
+         <h2>
+          {animal} - {breed} - {location}
+         </h2>
+        </div>
+      </a>
     );
   };
   
