f7526ce305fee151f0e6be64c89502c71b9504ea
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-05-01 12:17:30 -0500

    refactoring to react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
 <BrowserRouter>
-  <h1>Adopt Me!</h1>
-  <Routes>
-    <Route path="/details/:id" element={<Details />} />
-    <Route path="/" element={<SearchParams />} />
-  </Routes>
-  
-  
+  <QueryClientProvider client={queryClient}>
+    <header>
+      <Link to ='/'>Adopt Me!</Link>
+    </header>
+    <Routes>
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/" element={<SearchParams />} />
+    </Routes>
+  </QueryClientProvider>
 </BrowserRouter>
   );
 };
